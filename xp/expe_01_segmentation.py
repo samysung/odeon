d0b2315eebbd8c6498707472bc1844c72e6032ad
ngonthier <nicolas.gonthier@ign.fr>
2023-02-07 15:23:46 +0100

    for test on dl machine

--- xp/expe_01_segmentation.py
+++ xp/expe_01_segmentation.py
@@ -5,20 +5,29 @@ from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
+import os
+import sys
+sys.path.append('../odeon')
+
 from odeon.data.data_module import Input
 from odeon.models.change.module.change_unet import ChangeUnet
 
-root: str = '/media/HP-2007S005-data'
-root_dir: str = os.path.join(root, 'gers/change_dataset/patches')
+print('after load')
+#root: str = '/media/HP-2007S005-data'
+#root_dir: str = os.path.join(root, 'gers/change_dataset/patches')
+root: str = '/home/NGonthier/Documents/Detection_changement/data/'
+root_dir: str = os.path.join(root, 'gers/change/patches')
 fold_nb: int = 0
 fold: str = f'fold-{fold_nb}'
 root_fold: str = os.path.join(root_dir, fold)
 dataset: str = os.path.join(root_fold, 'train_split_0.geojson')
+batch_size = 2
 fit_params = {'input_fields': {"T0": {"name": "T0", "type": "raster", "dtype": "uint8", "band_indices": [1, 2, 3]},
                                "T1": {"name": "T1", "type": "raster", "dtype": "uint8", "band_indices": [1, 2, 3]},
                                "mask": {"name": "change", "type": "mask", "encoding": "integer"}},
                                'input_file': dataset,
-                               'root_dir': root_dir
+                               'root_dir': root_dir,
+                               'batch_size': batch_size
               }
 val_dataset: str = os.path.join(root_dir, 'val_split_0.geojson')
 val_params = {'input_fields': {"T0": {"name": "T0", "type": "raster", "dtype": "uint8", "band_indices": [1, 2, 3]},
@@ -30,11 +39,14 @@ val_params = {'input_fields': {"T0": {"name": "T0", "type": "raster", "dtype": "
 
 input = Input(fit_params=fit_params,
               validate_params=val_params)
+print('before model')
 model = ChangeUnet(model='fc_siam_conc')
+print('after model')
 path_model_checkpoint = ''
 save_top_k_models = 5
 path_model_log = ''
 accelerator = 'cpu'
+#accelerator = 'gpu'
 
 
 def main():
@@ -47,8 +59,11 @@ def main():
                                        monitor='val_iou')
     callbacks = [lr_monitor, model_checkpoint]
     logger = pl_loggers.TensorBoardLogger(save_dir=path_model_log)
-    trainer = Trainer(logger=logger, callbacks=callbacks, accelerator='cpu')
+    print('before trainer')
+    trainer = Trainer(logger=logger, callbacks=callbacks, accelerator=accelerator)
+    print('after trainer')
     trainer.fit(model=model, datamodule=input)
+    print('after fit')
 
 
 if __name__ == '__main__':
