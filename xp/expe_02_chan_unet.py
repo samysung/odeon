d261f83f935a8e7e4afbb7846d4b3cf989dd2cad
ngonthier <nicolas.gonthier@ign.fr>
2023-02-22 16:16:32 +0100

    for run exp

--- xp/expe_02_chan_unet.py
+++ xp/expe_02_chan_unet.py
@@ -24,6 +24,7 @@ fold_nb: int = 0
 fold: str = f'fold-{fold_nb}'
 root_fold: str = os.path.join(root_dir, fold)
 dataset: str = os.path.join(root_fold, 'train_split_'+str(fold_nb)+'.geojson')
+# The images here are not normalized between 0 and 1 should we do it ?
 batch_size = 2
 fit_params = {'input_fields': {"T0": {"name": "T0", "type": "raster", "dtype": "uint8", "band_indices": [1, 2, 3]},
                                "T1": {"name": "T1", "type": "raster", "dtype": "uint8", "band_indices": [1, 2, 3]},
@@ -60,7 +61,7 @@ accelerator = 'gpu' # 'cpu'
 limit_train_batches = 10
 limit_val_batches = 10
 limit_test_batches = 10
-max_epochs = 2
+max_epochs = 10
 check_val_every_n_epoch = 2
 log_every_n_steps =1
 def main():
